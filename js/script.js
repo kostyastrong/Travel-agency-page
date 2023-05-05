78260aed87afc6109e4d48fdc59c1fdf4a992dff
kostyastrong <kostyakostya872@gmail.com>
2023-05-05 12:05:50 +0300

    Add 30s alert cookie

--- js/script.js
+++ js/script.js
@@ -205,3 +205,46 @@ themeSwitcher.addEventListener('click', () => {
         switchSrcFolders(img, searchValue, replaceValue);
     })
 })
+
+
+// cookies
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function alertCookie() {
+    console.log('alert cookie = ', getCookie('alert'));
+    if (getCookie('alert') == null) {
+        setTimeout(() => {
+            alert('We are waiting a call from you!');
+            setCookie('alert', true, 1);
+        }, 30000); // 30s
+    }
+}
+
+alertCookie();
